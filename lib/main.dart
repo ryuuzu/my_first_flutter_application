c36a69067470915317d3050432405ee123d81dae
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-07-16 08:04:29 +0545

    added navigation menu for the application

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,39 @@ class MyAppState extends ChangeNotifier {
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+      children: [
+        SafeArea(
+            child: NavigationRail(
+          extended: false,
+          destinations: [
+            NavigationRailDestination(
+                icon: Icon(Icons.home), label: Text('Home')),
+            NavigationRailDestination(
+                icon: Icon(Icons.favorite), label: Text('Favourites')),
+          ],
+          selectedIndex: 0,
+          onDestinationSelected: (value) {
+            print('selected: $value');
+          },
+        )),
+        Expanded(
+          child: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: GeneratorPage(),
+          ),
+        )
+      ],
+    ));
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
