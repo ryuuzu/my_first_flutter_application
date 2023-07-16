5458278fa35028967a7f0c9d57de19750f5d44b0
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-07-16 08:05:06 +0545

    fixed container color not showing

--- lib/main.dart
+++ lib/main.dart
@@ -91,37 +91,35 @@ class GeneratorPage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold(
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          BigCard(pair: pair),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavourite();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like')),
-              SizedBox(
-                width: 10,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text(
-                    'Next',
-                    style: GoogleFonts.poppins(),
-                  )),
-            ],
-          )
-        ]),
-      ),
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        BigCard(pair: pair),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavourite();
+                },
+                icon: Icon(icon),
+                label: Text('Like')),
+            SizedBox(
+              width: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text(
+                  'Next',
+                  style: GoogleFonts.poppins(),
+                )),
+          ],
+        )
+      ]),
     );
   }
 }
