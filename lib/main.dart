a91c3d76d8f8a57d0fc1107bd7fb5dd8ff4b4aee
Utsav Gurmachhan <utsavmagar88728@gmail.com>
2023-07-16 07:37:13 +0545

    added button to get new word and update it

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,11 @@ class MainApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -36,7 +41,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(children: [
         Text('A random AWESOME idea:'),
-        Text(appState.current.asLowerCase)
+        Text(appState.current.asLowerCase),
+        ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            },
+            child: Text('Next'))
       ]),
     );
   }
